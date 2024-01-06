217724af418ae9445294f0e4104079440cccf0ac
Brian Guthrie <brianguthrie88@aol.com>
2024-01-06 14:45:33 -0500

    adding functionality that adds user input to local storage and displays it to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ var translatedTextEl = document.getElementById('translatedText');
 var modalContentEl = document.querySelector('.modal-content');
 var modalEl = document.querySelector('.modal');
 var modalCloseButtonEl = document.querySelector('.modal-close');
+var historyButtonSectionEl = document.querySelector('.hist-btn-div');
 
 
-function getApi(event) {
-    event.preventDefault();
+function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
     var requestUrl = 'https://text-translator2.p.rapidapi.com/translate';
     var options = {
@@ -57,11 +57,73 @@ function getApi(event) {
     }  
 }
 
+function readItemsFromStorage() {
+    var items = localStorage.getItem('items');
+    if (items) {
+      items = JSON.parse(items);
+    } else {
+      items = [];
+    }
+    return items;
+  }
+
+  //this function makes the items that are set to local storage strings so that they can be read by local storage
+function saveItemsToStorage(items) {
+    console.log(items);
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+  //this function handles saving each task to the local storage as an array of objects
+function handleSaveItem() {
+    var value = inputTextEl.value.trim();
+    var items = readItemsFromStorage();
+    if(!languageSelectEl.value) {
+        modalEl.classList.add('is-active');
+        modalContentEl.textContent = "Please select a language to translate into.";
+    } else if(!inputTextEl.value) {
+        modalEl.classList.add('is-active');
+        modalContentEl.textContent = "Please input text you want to translate.";
+        translatedTextEl.textContent = '';
+        languageSelectEl.value = '';
+    } else {
+        items.push(value);
+        saveItemsToStorage(items);
+        getItemsFromStorage();
+    }
+}
+
+function handleTranslateClick() {
+    getApi();
+    handleSaveItem();
+    getItemsFromStorage();
+}
+
+  //this funtion is used to get the items from and then paste them to the page when the app is reopened
+function getItemsFromStorage() {
+    historyButtonSectionEl.replaceChildren();
+    var items = readItemsFromStorage();
+    for (var i =0; i < items.length; i++) {
+        if(items.length > 10) {
+            items.shift();
+            saveItemsToStorage(items);
+        } else {
+            var historyButtonEl = document.createElement('button');
+            historyButtonEl.textContent = items[i];
+            historyButtonSectionEl.appendChild(historyButtonEl);
+            historyButtonEl.addEventListener('click', function() {
+            inputTextEl.value = this.textContent;
+            translatedTextEl.textContent = '';
+        });
+        }
+    }
+}
+
 function closeModal() {
     modalEl.classList.remove('is-active');
 }
 
 modalCloseButtonEl.addEventListener('click', closeModal);
 
-translateButtonEL.addEventListener('click', getApi);
+translateButtonEL.addEventListener('click', handleTranslateClick);
 
+getItemsFromStorage();
